7163e6c53abc43f7fcbd21a5219ff8cf70860275
a.cruzgabriel98@gmail.com <a.cruzgabriel98@gmail.com>
2023-03-31 14:27:28 -0700

    small game done

--- test.c
+++ test.c
@@ -1,10 +1,16 @@
-
+#include<stdio.h>
 
 int main(){
     int secrectNumber = 5;
     int guess;
 
-    
+    // while guess is not equal to number 
+    while(guess != secrectNumber){
+        printf("Enter a number: ");
+        scanf("%d", & guess);
+    };
+    printf("YOU WIN");
 
     return 0;
 }
+
